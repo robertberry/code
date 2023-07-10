2688856548322656e170be8f33d5d83bb5e756a7
Robert Berry <rjberry@gmail.com>
2023-07-09 20:48:31 -0400

    Add skeleton Bazel project for Labyrinths

---
+++ labyrinths/labyrinths.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char **argv) {
+  std::cout << "Labyrinths" << std::endl;
+  std::cout << "© Robert Berry, 2023" << std::endl;
+  std::cout << std::endl;
+
+  // TODO(robertberry): Implement game.
+
+  return 0;
+}
